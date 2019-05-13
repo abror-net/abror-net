321e175c23f7aedb7e46d6748058a55fbfdedfff
Muhammad Muhlas <muhammadmuhlas3@gmail.com>
2019-05-14 04:37:47 +0700

    styling pict
    Signed-off-by: Muhammad Muhlas <muhammadmuhlas3@gmail.com>

--- source/about.blade.php
+++ source/about.blade.php
@@ -12,7 +12,7 @@
 
     <img src="/assets/img/muhammadmuhlas.jpg"
         alt="About image"
-        class="flex rounded-full bg-contain mx-auto md:float-right my-6 md:ml-10" style="width: 29rem;">
+        class="flex rounded-full bg-contain mx-auto md:float-right my-6 md:ml-10" style="width: 14rem;margin-top: 12px;margin-left: 24px;">
     <p class="mb-6">
         <p class="mb-6"><span style="color: #0174d4">[NOTICE]</span> Hello visitor, my name is Muhammad Muhlas Abror</p>
         <p class="mb-6"><span style="color: #0174d4">[NOTICE]</span> I don't know how you can reach my site</p>
