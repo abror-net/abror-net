d9d548fed4660c22899e69a32755bc2527800a4c
Muhammad Muhlas <muhammadmuhlas3@gmail.com>
2019-05-14 04:22:02 +0700

    about
    Signed-off-by: Muhammad Muhlas <muhammadmuhlas3@gmail.com>

--- source/about.blade.php
+++ source/about.blade.php
@@ -10,10 +10,14 @@
 @section('body')
     <h1>About</h1>
 
-    <img src="/assets/img/about.png"
+    <img src="https://lh3.googleusercontent.com/y-HaP8FYnPHC0ds9qqkDXVCZx3DoPVwsodznSa9VbVqIoL8hfQ8fvhy1pgeyqJEpHVJzaEz51vznBqT4m5vwKFdb_zPCJqgfl_-whT4bH-HiElHRCcaiFxgTFMybQaGksp_EXvbnAs8=w1200"
         alt="About image"
-        class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
-
-    <p class="mb-6">Muhammad Muhlas Abror</p>
-    <p class="mb-6"><a href="mailto:muhammadmuhlas3@gmail.com">Sent me an email!</p>
+        class="flex rounded-full bg-contain mx-auto md:float-right my-6 md:ml-10" style="width: 29rem;">
+    <p class="mb-6">
+        <p class="mb-6"><span style="color: #0174d4">[NOTICE]</span> Hello visitor, my name is Muhammad Muhlas Abror</p>
+        <p class="mb-6"><span style="color: #0174d4">[NOTICE]</span> I don't know how you can reach my site</p>
+        <p class="mb-6"><span style="color: #0174d4">[NOTICE]</span> Anyway, thank you</p>
+        <p class="mb-6"><span style="color: #0174d4">[NOTICE]</span> I'm the owner of this site</p>
+        <p class="mb-6"><span style="color: #0174d4">[NOTICE]</span> For more info, mail me at <a href="mailto:muhammadmuhlas3@gmail.com">muhammadmuhlas3@gmail.com</p>
+    </p>
 @endsection
